9feb6d0fd00eaf6d8ce2fd01f11257db0cbc99b9
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:07:30 +0400

    PaymentMethod Service done

--- src/main/java/com/example/Travel/booking/system/Service/PaymentMethodService.java
+++ src/main/java/com/example/Travel/booking/system/Service/PaymentMethodService.java
@@ -1,4 +1,18 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.PaymentMethod;
+import com.example.Travel.booking.system.Repository.PaymentMethodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PaymentMethodService {
+    @Autowired
+    PaymentMethodRepository paymentMethodRepository;
+
+    public List<PaymentMethod> getAllPaymentMethod(){
+        return paymentMethodRepository.findAll();
+    }
 }
