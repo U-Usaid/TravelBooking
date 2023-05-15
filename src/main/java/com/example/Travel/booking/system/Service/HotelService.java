9092efbbe32c2cf419c911c990ec1282641c7bf9
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:06:52 +0400

    Hotel Service done

--- src/main/java/com/example/Travel/booking/system/Service/HotelService.java
+++ src/main/java/com/example/Travel/booking/system/Service/HotelService.java
@@ -1,4 +1,19 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.Flight;
+import com.example.Travel.booking.system.Model.Hotel;
+import com.example.Travel.booking.system.Repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class HotelService {
+    @Autowired
+    HotelRepository hotelRepository;
+
+    public List<Hotel> getAllHotel(){
+        return hotelRepository.findAll();
+    }
 }
