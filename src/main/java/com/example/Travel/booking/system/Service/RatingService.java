4b9d6372794e30900386d5eef5772cbdaf06f1f7
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:07:45 +0400

    PaymentMethod Service done

--- src/main/java/com/example/Travel/booking/system/Service/RatingService.java
+++ src/main/java/com/example/Travel/booking/system/Service/RatingService.java
@@ -1,4 +1,18 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.Rating;
+import com.example.Travel.booking.system.Repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RatingService {
+    @Autowired
+    RatingRepository ratingRepository;
+
+    public List<Rating> getALlRating(){
+        return ratingRepository.findAll();
+    }
 }
