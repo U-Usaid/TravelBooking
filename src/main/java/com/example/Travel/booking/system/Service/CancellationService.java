1117e267889ce7dab82e016d422e587d833b7be8
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:05:03 +0400

    Cancellation Service done

--- src/main/java/com/example/Travel/booking/system/Service/CancellationService.java
+++ src/main/java/com/example/Travel/booking/system/Service/CancellationService.java
@@ -1,4 +1,18 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.Cancellation;
+import com.example.Travel.booking.system.Repository.CancellationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CancellationService {
+    @Autowired
+    CancellationRepository cancellationRepository;
+
+    public List<Cancellation> getAllCancellation(){
+        return cancellationRepository.findAll();
+    }
 }
