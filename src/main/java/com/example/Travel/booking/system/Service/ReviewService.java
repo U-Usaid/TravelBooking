c5d03aed2c6c15046118c415cc4c3366146f0f0f
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:08:17 +0400

    Review Service done

--- src/main/java/com/example/Travel/booking/system/Service/ReviewService.java
+++ src/main/java/com/example/Travel/booking/system/Service/ReviewService.java
@@ -1,5 +1,19 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.Review;
+import com.example.Travel.booking.system.Repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReviewService {
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    public List<Review> getALlReview(){
+        return reviewRepository.findAll();
+    }
 
 }
