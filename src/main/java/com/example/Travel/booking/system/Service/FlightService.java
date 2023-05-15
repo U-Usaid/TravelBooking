a298dc80c0bf049d9f5de4c0e4a98934707981ee
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:06:26 +0400

    Flight Service done

--- src/main/java/com/example/Travel/booking/system/Service/FlightService.java
+++ src/main/java/com/example/Travel/booking/system/Service/FlightService.java
@@ -1,4 +1,18 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.Flight;
+import com.example.Travel.booking.system.Repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FlightService {
+    @Autowired
+    FlightRepository flightRepository;
+
+    public List<Flight> getAllFlight(){
+        return flightRepository.findAll();
+    }
 }
