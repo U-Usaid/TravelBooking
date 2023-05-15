630901f8b76e920b14cdc829bba8e75ddfc8d859
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:05:58 +0400

    Customer Service done

--- src/main/java/com/example/Travel/booking/system/Service/CustomerService.java
+++ src/main/java/com/example/Travel/booking/system/Service/CustomerService.java
@@ -1,4 +1,17 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.Customer;
+import com.example.Travel.booking.system.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+    public List<Customer> getAllCustomer(){
+        return customerRepository.findAll();
+    }
 }
