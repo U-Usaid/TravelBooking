e17747384c5279e6d069099dbb92d7fced22b73c
U-Usaid <osaid.osaid66@gmail.com>
2023-05-15 11:05:31 +0400

    CarRental Service done

--- src/main/java/com/example/Travel/booking/system/Service/CarRentalService.java
+++ src/main/java/com/example/Travel/booking/system/Service/CarRentalService.java
@@ -1,4 +1,18 @@
 package com.example.Travel.booking.system.Service;
 
+import com.example.Travel.booking.system.Model.CarRental;
+import com.example.Travel.booking.system.Repository.CarRentalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarRentalService {
+    @Autowired
+    CarRentalRepository carRentalRepository;
+
+    public List<CarRental> getAllCarRental(){
+        return carRentalRepository.findAll();
+    }
 }
